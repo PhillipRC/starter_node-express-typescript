9a7e57482d8a5247b9da4c3bd21c6e0dd123c78b
pcargo <phillip.r.cargo@gmail.com>
2018-03-28 06:53:01 -0400

    Added express server example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Howdy');
+import express from "express";
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.json({
+        message: 'Howdy World!'
+    })
+});
+
+app.listen(8081);
